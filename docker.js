5dc0e8c2e240c43e68f4244cfacf279c7b231ab6
zhananzhangsan <77868385+zhananzhangsan@users.noreply.github.com>
2025-10-30 10:57:43 +0800

    Create docker.js

---
+++ docker.js
@@ -0,0 +1,16 @@
+docker pull  path || '/cloud/profile/o3MkOsLzt'
+  ghcr.io/zhananzhangsan/argonode:latest
+    SUB_PATH || 'sub';       
+    PORT || 3000;      
+    UUID || ''; 
+    ARGO_DOMAIN || '';          
+    ARGO_AUTH || '';              
+    ARGO_PORT || 8001
+  docker.io/blug/argo-x:latest 
+    PORT || 3000;      
+    UUID || ''; 
+    ARGO_DOMAIN || '';         
+    ARGO_AUTH || '';              
+    ARGO_PORT || 8080
+    WSPATH || 'cloud'
+  docker.io/blueg/xhttp
