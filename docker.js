13d07d23e44f0bca098319d0203e5175395e1d17
zhananzhangsan <77868385+zhananzhangsan@users.noreply.github.com>
2026-04-04 20:23:45 +0800

    Update docker.js

--- docker.js
+++ docker.js
@@ -54,7 +54,7 @@ function getRandomArray(array) {
     ARGO_PORT || 8080
     WSPATH || 'cloud'
 
-  docker.io/blueg/xhttp:basic  //basic-标准版 //ipv4first-自定义dns版
+  docker.io/blueg/xhttp:latest  //"ipv4first","01","latest"
     3000
     DOMAIN || ''
     UUID || ''
